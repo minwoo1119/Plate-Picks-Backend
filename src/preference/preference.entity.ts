935d9edcf43549d8f85d588ba1d4cea30ae1cf76
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-06-15 21:37:15 +0900

    [Feat] Preference 엔티티 생성

---
+++ src/preference/preference.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+import { Food } from 'src/food/food.entity';
+import { Participants } from 'src/participants/participants.entity';
+
+@Entity()
+export class Preference {
+  @PrimaryColumn('uuid')
+  participant_id: string;
+  @PrimaryColumn('uuid')
+  food_id: string;
+  @ManyToOne(() => Participants, (participant) => participant.preference, {
+    onDelete: 'CASCADE',
+  })
+  participant: Participants;
+
+  @ManyToOne(() => Food, (food) => food.preferences, {
+    onDelete: 'CASCADE',
+  })
+  food: Food;
+
+  @Column({ type: 'enum', enum: ['Good', 'Soso', 'Bad'], default: 'Soso' })
+  rating: 'Good' | 'Soso' | 'Bad';
+
+  @CreateDateColumn()
+  responsed_at: Date;
+}
