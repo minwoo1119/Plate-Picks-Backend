cb5f3103dedee2a58199a316483b86cdadc7bfe8
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-05 15:25:40 +0900

    [Chore] CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(7777);
 }
 bootstrap();
