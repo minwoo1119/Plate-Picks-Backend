664e863cc2e6fb6d5c9c0b9317af8e9efb84d072
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-05 16:15:04 +0900

    [Feat] 방 기준 참가자 조회 api 구현

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -24,4 +24,9 @@ export class RoomController {
   async getRoomStatus(@Param('code') code: string) {
     return this.roomService.getRoomStatus(code);
   }
+
+  @Get('participants')
+  async getAllParticipants(@Param('code') code: string) {
+    return this.roomService.getRoomParticipants(code);
+  }
 }
