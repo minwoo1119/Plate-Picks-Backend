94f5ab0453e29b596e54e6bf17b7f53a825c4fa1
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-05 14:05:59 +0900

    [Fix] port 번호 수정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(12345);
+  await app.listen(7777);
 }
 bootstrap();
